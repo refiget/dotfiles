dc1e8d6352a063841e410461ab8eea4847482beb
Bob mini <bob+macmini@example.com>
2026-01-29 18:18:00 +0800

    Version 1.1

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -81,6 +81,18 @@ keymap("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", { silent = true, desc 
 keymap("n", "<leader>cd", "<cmd>Lspsaga show_line_diagnostics<CR>", { silent = true, desc = "Show line diagnostics" })
 keymap("n", "<leader>xx", "<cmd>TroubleToggle<CR>", { silent = true, desc = "Toggle trouble" })
 
+-- DAP (Debugging) - Function keys
+keymap("n", "<F5>", function() require("dap").continue() end, { silent = true, desc = "DAP continue" })
+keymap("n", "<F10>", function() require("dap").step_over() end, { silent = true, desc = "DAP step over" })
+keymap("n", "<F11>", function() require("dap").step_into() end, { silent = true, desc = "DAP step into" })
+keymap("n", "<F12>", function() require("dap").step_out() end, { silent = true, desc = "DAP step out" })
+keymap("n", "<F9>", function() require("dap").toggle_breakpoint() end, { silent = true, desc = "DAP toggle breakpoint" })
+keymap("n", "<leader>dB", function()
+  require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+end, { silent = true, desc = "DAP conditional breakpoint" })
+keymap("n", "<leader>dr", function() require("dap").repl.toggle() end, { silent = true, desc = "DAP REPL" })
+keymap("n", "<leader>du", function() require("dapui").toggle() end, { silent = true, desc = "DAP UI toggle" })
+
 
 -- Compile/Run
 -- ii-0o-9i
