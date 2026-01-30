ac0f8ddf99ed3283b7bc5e9d61a09b6ac36e06ce
Bob mini <bob+macmini@example.com>
2026-01-30 23:19:22 +0800

    Version 1.44

--- nvim/lua/plugin_config/init.lua
+++ nvim/lua/plugin_config/init.lua
@@ -1,7 +1,6 @@
 -- Plugin configuration module initialization
 require("plugin_config.cmp")
 require("plugin_config.lsp")
-require("plugin_config.treesitter")
 require("plugin_config.telescope")
 require("plugin_config.dap")
 require("plugin_config.appearance")
