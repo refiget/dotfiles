35fb0d9ee07d9161eb9153086cd3f6e288a5eabb
Bob mini <bob+macmini@example.com>
2026-01-30 11:37:44 +0800

    Version 1.21

--- nvim/lua/user/keymaps/lsp.lua
+++ nvim/lua/user/keymaps/lsp.lua
@@ -3,7 +3,7 @@ local keymap = vim.keymap.set
 keymap("n", "gd", "<cmd>Lspsaga goto_definition<CR>", { silent = true, desc = "Go to definition" })
 keymap("n", "gi", "<cmd>Lspsaga goto_implementation<CR>", { silent = true, desc = "Go to implementation" })
 keymap("n", "gr", "<cmd>Lspsaga lsp_finder<CR>", { silent = true, desc = "Go to references" })
-keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>", { silent = true, desc = "Show hover information" })
+keymap("n", "<leader>K", "<cmd>Lspsaga hover_doc<CR>", { silent = true, desc = "Show hover information" })
 keymap("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", { silent = true, desc = "Code action" })
 keymap("n", "<leader>cd", "<cmd>Lspsaga show_line_diagnostics<CR>", { silent = true, desc = "Show line diagnostics" })
 keymap("n", "<leader>xx", "<cmd>TroubleToggle<CR>", { silent = true, desc = "Toggle trouble" })
