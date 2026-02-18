f9c47672b0f7dcff37bd117ea1cbf371483a96f1
bob <bob@Bobs-MacBook-Air.local>
2026-02-19 04:09:50 +0800

    nvim: avoid keymap conflict (diagnostics float -> <leader>ld)

--- nvim/lua/keymaps/misc.lua
+++ nvim/lua/keymaps/misc.lua
@@ -4,6 +4,7 @@ local opts = { silent = true }
 keymap("n", "<leader>sw", ":set wrap!<CR>", { desc = "Toggle wrap" })
 
 -- Diagnostics (float)
-keymap("n", "<leader>e", function()
+-- Keep <leader>e for nvim-tree.
+keymap("n", "<leader>ld", function()
   vim.diagnostic.open_float(nil, { focus = false })
 end, { desc = "Diagnostics: float" })
