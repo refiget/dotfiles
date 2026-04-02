87ed2f1dcc12c0e326f7fd27dc4b6f29dea9fe84
Bob mini <bob+macmini@example.com>
2026-04-02 23:53:46 +0800

    nvim: disable flash remote r/R and keep s jump mapping

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -1,6 +1,11 @@
 return {
   {
     "folke/flash.nvim",
+    -- Keep default s/S behavior, disable remote r/R.
+    keys = {
+      { "r", mode = "o", false },
+      { "R", mode = { "o", "x" }, false },
+    },
     opts = {
       modes = {
         char = {
