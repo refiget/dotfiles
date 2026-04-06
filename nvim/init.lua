66c8b309233ec642ab52baab3035eeb750e24fa7
Bob mini <bob+macmini@example.com>
2026-04-06 23:52:12 +0800

    nvim: make J/K 5-line motions apply at startup (before VeryLazy)

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,9 @@
+-- Early core keymaps (must be available before VeryLazy)
+local map = vim.keymap.set
+local opts = { silent = true, noremap = true }
+map("n", "J", "5j", opts)
+map("n", "K", "5k", opts)
+map("x", "J", "5j", opts)
+map("x", "K", "5k", opts)
+
 require("config.lazy")
