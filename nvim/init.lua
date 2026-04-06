7ed456eec9118349c9ce2dc5901989980e1b8d87
Bob mini <bob+macmini@example.com>
2026-04-07 00:03:23 +0800

    nvim: enforce K/J 5-line motions on LspAttach buffer maps

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,4 +6,15 @@ map("n", "K", "5k", opts)
 map("x", "J", "5j", opts)
 map("x", "K", "5k", opts)
 
+-- Keep K/J behavior even after LSP buffer-local mappings attach
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local b = args.buf
+    vim.keymap.set("n", "J", "5j", { buffer = b, silent = true, noremap = true })
+    vim.keymap.set("n", "K", "5k", { buffer = b, silent = true, noremap = true })
+    vim.keymap.set("x", "J", "5j", { buffer = b, silent = true, noremap = true })
+    vim.keymap.set("x", "K", "5k", { buffer = b, silent = true, noremap = true })
+  end,
+})
+
 require("config.lazy")
